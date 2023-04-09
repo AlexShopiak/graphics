9cb426c4d810221ad8aaad991b947d8b2e0ff16e
Alex Shopiak <alshop2004@gmail.com>
2023-04-09 10:21:09 +0200

    Refactor main() func & utils

--- app.js
+++ app.js
@@ -1,30 +1,29 @@
 'use strict'
 const prompt = require('prompt-sync')();
-const {exprIsValid, exprIsAlgebraic, valueIsValid} = require('./lib/validators/validators.js');
+const {validateExpr, validateValue, exprIsAlgebraic} = require('./lib/utils.js');
 const {parseAndCompute} = require('./lib/parser/parser.js');
 
 const main = async () => {
     let expr;
     while (1) {
-        expr = prompt('Enter expression: ');
-        if(exprIsValid(expr)) {
-            break;
-        }
-        console.log('Invalid format. Try again');
-    }  
-
-    if (exprIsAlgebraic(expr)){
-        let value;
-        while (1) {
-            value = prompt('Enter value: ');
-            if(valueIsValid(value)) {
-                break;
-            }
-            console.log('Invalid format. Try again');
-        } 
-        const variable = 'x';//todo
-        expr = expr.replaceAll(variable, value);
+      expr = prompt("Enter expression: ");
+      const status = validateExpr(expr);
+      if (status == 'valid') break;
+      console.log(status);
     }
+    
+    if (exprIsAlgebraic(expr)) {
+      let value;
+      while (1) {
+        value = prompt("Enter value: ");
+        const status = validateValue(expr);
+      if (status == 'valid') break;
+      console.log(status);
+      }
+      const variable = "x"; //todo
+      expr = expr.replaceAll(variable, value);
+    }
+  
     const result = parseAndCompute(expr);
     console.log(result);
 }
