fb47aa155a584132dcc29b2f7842b4de0cc32a0b
Alex Shopiak <alshop2004@gmail.com>
2023-05-28 17:03:14 +0200

    Rewrite utils in OOP

--- app.js
+++ app.js
@@ -1,31 +1,37 @@
 'use strict'
 const prompt = require('prompt-sync')();
-const {validateExpr, validateValue, searchVariable} = require('./lib/utils.js');
-const {parseAndCompute} = require('./lib/parser/parser.js');
+const {Validator} = require('./lib/validator.js');
+const {Parser} = require('./lib/parser/parser.js');
+
+const v = new Validator;
+const p = new Parser;
 
 const main = () => {
-    let expr;
+  //Ask for expression
+  let expr;
+  while (1) {
+    expr = prompt("Enter expression: ");
+    expr = v.normaliseExpr(expr);
+    const status = v.validateExpr(expr);
+    if (status == 'valid') break;
+    console.log(status);
+  }
+  
+  //Ask for values
+  const variable = v.searchVariable(expr);
+  if (variable != 'null') {
+    let value;
     while (1) {
-      expr = prompt("Enter expression: ");
-      expr = expr.replaceAll(" ","");
-      const status = validateExpr(expr);
+      value = prompt("Enter value: ");
+      const status = v.validateValue(value);
       if (status == 'valid') break;
       console.log(status);
     }
-    
-    const variable = searchVariable(expr);
-    if (variable != 'null') {
-      let value;
-      while (1) {
-        value = prompt("Enter value: ");
-        const status = validateValue(value);
-        if (status == 'valid') break;
-        console.log(status);
-      }
-      expr = expr.replaceAll(variable, value);
-    }
+    expr = expr.replaceAll(variable, value);
+  }
   
-    const result = parseAndCompute(expr);
-    console.log(result);
+  //Parse
+  const result = p.parseAndCompute(expr);
+  console.log(result);
 }
 main();
