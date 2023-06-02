0d1af584d64b71b2c8b9eaa9d620d0eac5c363d9
Alex Shopiak <alshop2004@gmail.com>
2023-06-03 00:36:13 +0200

    Remove magic strings

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const prompt = require('prompt-sync')();
 const {Validator} = require('./lib/utils/validator.js');
 const {Compiler} = require('./lib/compiler/compiler.js');
 
+const CODE_QUIT = 'q';
+const CODE_EXPR = 'e';
+
 const v = new Validator;
 const c = new Compiler;
 
@@ -12,7 +15,7 @@ const askExpr = () => {
     expr = prompt("Enter expression: ");
     expr = v.normaliseExpr(expr);
 
-    if (expr == 'q') break;
+    if (expr == CODE_QUIT) break;
     const status = v.validateExpr(expr);
     if (status == 'valid') break;
 
@@ -27,7 +30,7 @@ const askValue = () => {
     value = prompt("Enter value: ");
     value = v.normaliseValue(value);
 
-    if (value == 'q'|| value == 'e') break;
+    if (value == CODE_QUIT|| value == CODE_EXPR) break;
     const status = v.validateValue(value);
     if (status == 'valid') break;
 
@@ -39,14 +42,14 @@ const askValue = () => {
 const main = () => {
   let value = 0;
   let expr = askExpr();
-  if (expr == 'q') process.exit(0);
+  if (expr == CODE_QUIT) process.exit(0);
   c.compile(expr);
 
   if (expr.includes('x')) {
     while (1) {
       value = askValue();
-      if (value == 'q') process.exit(0);
-      if (value == 'e') break;
+      if (value == CODE_QUIT) process.exit(0);
+      if (value == CODE_EXPR) break;
 
       const result = c.compute({x:Number(value)});
       console.log("Result: ", result);
