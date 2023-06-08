6d393283aff5cdb42fee9228928c08bad7906a7a
Alex Shopiak <alshop2004@gmail.com>
2023-06-08 04:18:40 +0200

    Update Errors & Implement compiler tests

--- test/compiler.test.js
+++ test/compiler.test.js
@@ -3,4 +3,50 @@
 const {Compiler} = require('../lib/compiler/compiler.js');
 const c = new Compiler;
 
-test('stub', () => {})
+test('results and aoperations', () => {
+    c.compile('5 + 14');
+    let res = c.compute(4);
+    expect(res).toEqual(19);
+
+    c.compile('x + 1');
+    res = c.compute(4);
+    expect(res).toEqual(5);
+
+    c.compile('x - 2');
+    res = c.compute(4);
+    expect(res).toEqual(2);
+
+    c.compile('x * 2');
+    res = c.compute(4);
+    expect(res).toEqual(8);
+
+    c.compile('x / 2');
+    res = c.compute(4);
+    expect(res).toEqual(2);
+
+    c.compile('x ^ 2');
+    res = c.compute(4);
+    expect(res).toEqual(16);
+})
+
+test('Braces', () => {
+    c.compile('(5)');
+    let res = c.compute(4);
+    expect(res).toEqual(5);
+
+    c.compile('((x) + 1)');
+    res = c.compute(4);
+    expect(res).toEqual(5);
+})
+
+test('Errors', () => {
+    c.compile('sim(x)');
+    expect(() => c.compute()).toThrow('Unspecified identifier "sim"');
+
+    c.compile('4 + y');
+    expect(() => c.compute()).toThrow('Unspecified identifier "y"');
+
+    expect(() => c.compile('(')).toThrow("Malformed expression");
+    expect(() => c.compile('x+5-/')).toThrow('Malformed expression');
+    expect(() => c.compile('*')).toThrow('Malformed expression');
+})
